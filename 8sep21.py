5a7c4e83a476c997b2c6a742443fa367ddd22d2b
lubarreno <90362453+lubarreno@users.noreply.github.com>
2021-09-08 20:49:51 -0500

    Create 8sep21.py
    Se carga lo solicitado

---
+++ 8sep21.py
@@ -0,0 +1,13 @@
+#en la clase del día 7 de septiembre nos ensañaron a utilizar
+#el comando print, que imprime en consola lo que necesito visualizar
+
+#tipe me permite inspeccionar el tipo de dato
+
+print("Realizado por Lucy Barreno")
+print(type(875))
+print(type(4.89))
+print(type("Now is better than never"))
+print(type(1.32))
+print(type(5 + 8j) is int)
+print(type(8.2) is int)
+print(type("Readability counts") is str)
